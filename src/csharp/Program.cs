20adadce33e0a9305c509e4ec15795adc7f2c079
Kyungjae Park <kyungjae.park@gmail.com>
2015-02-07 07:38:25 +0900

    using BitmapData instead of GetPixel/SetPixels

--- src/csharp/Program.cs
+++ src/csharp/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace TransparencyExtractor
 {
@@ -43,40 +45,62 @@ namespace TransparencyExtractor
             Bitmap i_black = new Bitmap(blackBackgroundImagePath);
             Bitmap i_result = new Bitmap(i_white.Width, i_white.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
-            for (int y = 0; y < i_white.Height; y++)
+            Rectangle imageRect = new Rectangle(0, 0, i_white.Width, i_white.Height);
+
+            BitmapData bd_white = i_white.LockBits(imageRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData bd_black = i_black.LockBits(imageRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData bd_result = i_result.LockBits(imageRect, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            long ptr_white = bd_white.Scan0.ToInt64();
+            long ptr_black = bd_black.Scan0.ToInt64();
+            long ptr_result = bd_result.Scan0.ToInt64();
+
+            byte[] buf_white = new byte[imageRect.Width * 3];
+            byte[] buf_black = new byte[imageRect.Width * 3];
+            byte[] buf_result = new byte[imageRect.Width * 4];
+
+            for (int y = 0; y < imageRect.Height; y++)
             {
-                for (int x = 0; x < i_white.Width; x++)
+                Marshal.Copy(new IntPtr(ptr_white + y * bd_white.Stride), buf_white, 0, imageRect.Width * 3);
+                Marshal.Copy(new IntPtr(ptr_black + y * bd_black.Stride), buf_black, 0, imageRect.Width * 3);
+                for (int x = 0; x < imageRect.Width; x++)
                 {
                     // calc pixel
-                    uint whiteARGB = (uint)Color.White.ToArgb();
-                    uint blackARGB = (uint)Color.Black.ToArgb();
-
-                    uint argb_white = (uint)i_white.GetPixel(x, y).ToArgb();
-                    uint argb_black = (uint)i_black.GetPixel(x, y).ToArgb();
+                    byte r_white = buf_white[x * 3 + 2];
+                    byte g_white = buf_white[x * 3 + 1];
+                    byte b_white = buf_white[x * 3 + 0];
+                    byte r_black = buf_black[x * 3 + 2];
+                    byte g_black = buf_black[x * 3 + 1];
+                    byte b_black = buf_black[x * 3 + 0];
 
                     Color c_result = Color.Transparent;
-                    if (argb_white == whiteARGB && argb_black == blackARGB)
+                    if (r_white == 0xff && g_white == 0xff && b_white == 0xff && r_black == 0 && g_black == 0 && b_black == 0)
                     {
-                        c_result = Color.Transparent;
+                        buf_result[x * 4 + 3] = 0;
+                        buf_result[x * 4 + 2] = 0;
+                        buf_result[x * 4 + 1] = 0;
+                        buf_result[x * 4 + 0] = 0;
                     }
-                    else if (argb_white == argb_black)
+                    else if (r_white == r_black && g_white == g_black && b_white == b_black)
                     {
-                        c_result = Color.FromArgb((int)argb_white);
+                        buf_result[x * 4 + 3] = 0xff;
+                        buf_result[x * 4 + 2] = r_white;
+                        buf_result[x * 4 + 1] = g_white;
+                        buf_result[x * 4 + 0] = b_white;
                     }
                     else
                     {
-                        uint r_white = ((argb_white >> 16) & 0xff);
-                        uint g_white = ((argb_white >> 8) & 0xff);
-                        uint b_white = ((argb_white >> 0) & 0xff);
-                        uint r_black = ((argb_black >> 16) & 0xff);
-                        uint g_black = ((argb_black >> 8) & 0xff);
-                        uint b_black = ((argb_black >> 0) & 0xff);
-
-                        uint colorAlphaMap_r = colorAlphaMap[(r_white << 8) | (r_black)];
-                        uint colorAlphaMap_g = colorAlphaMap[(g_white << 8) | (g_black)];
-                        uint colorAlphaMap_b = colorAlphaMap[(b_white << 8) | (b_black)];
+                        uint colorAlphaMap_r = colorAlphaMap[(((uint)r_white) << 8) | (r_black)];
+                        uint colorAlphaMap_g = colorAlphaMap[(((uint)g_white) << 8) | (g_black)];
+                        uint colorAlphaMap_b = colorAlphaMap[(((uint)b_white) << 8) | (b_black)];
                         uint alpha = Math.Min(Math.Min(colorAlphaMap_r & 0xff00, colorAlphaMap_g & 0xff00), colorAlphaMap_b & 0xff00);
                         alpha = (alpha >> 8) & 0xff;
+
+                        buf_result[x * 4 + 3] = (byte)alpha;
+                        buf_result[x * 4 + 2] = (byte)(colorAlphaMap_r & 0xff);
+                        buf_result[x * 4 + 1] = (byte)(colorAlphaMap_g & 0xff);
+                        buf_result[x * 4 + 0] = (byte)(colorAlphaMap_b & 0xff);
+
                         uint argb_result = ((alpha) << 24)
                             | ((colorAlphaMap_r & 0xff) << 16)
                             | ((colorAlphaMap_g & 0xff) << 8)
@@ -84,9 +108,14 @@ namespace TransparencyExtractor
 
                         c_result = Color.FromArgb((int)argb_result);
                     }
-                    i_result.SetPixel(x, y, c_result);
                 }
+                Marshal.Copy(buf_result, 0, new IntPtr(ptr_result + y * bd_result.Stride), imageRect.Width * 4);
             }
+
+            i_white.UnlockBits(bd_white);
+            i_black.UnlockBits(bd_black);
+            i_result.UnlockBits(bd_result);
+
             i_result.Save(resultImagePath);
         }
     }
